03ffb0282c42ff9afcda764cc1ec766c8427bb1f
Priya-pavani <112808827+Priya-pavani@users.noreply.github.com>
2023-06-06 22:57:52 +0530

    Update imageviewerapp.py
    here in this commit I have updated the color of the next and back buttons and I have also added the scroll bar

--- imageviewerapp.py
+++ imageviewerapp.py
@@ -15,54 +15,54 @@ root.configure(background="lightblue")
 root.title("Image viewer")
 #root.attributes("-fullscreen", True)
 
-width = 600
-height = 450
+width = 500
+height = 500
 
-img0 = Image.open("tkinterpractice\img0.jpg")
+img0 = Image.open("tkinterpractice\imageviewerapp\img0.jpg")
 img0 = img0.resize((width, height))
 img0 = ImageTk.PhotoImage(img0)
 
-img1 = Image.open("tkinterpractice\img1.jpg")
+img1 = Image.open("tkinterpractice\imageviewerapp\img1.jpg")
 img1 = img1.resize((width, height))
 img1 = ImageTk.PhotoImage(img1)
 
-img2 = Image.open("tkinterpractice\img2.jpg")
+img2 = Image.open("tkinterpractice\imageviewerapp\img2.jpg")
 img2 = img2.resize((width, height))
 img2 = ImageTk.PhotoImage(img2)
 
-img3 = Image.open("tkinterpractice\img3.jpg")
+img3 = Image.open("tkinterpractice\imageviewerapp\img3.jpg")
 img3 = img3.resize((width, height))
 img3 = ImageTk.PhotoImage(img3)
 
-img4 = Image.open("tkinterpractice\img4.jpg")
+img4 = Image.open("tkinterpractice\imageviewerapp\img4.jpg")
 img4 = img4.resize((width, height))
 img4 = ImageTk.PhotoImage(img4)
 
-img5 = Image.open("tkinterpractice\img5.jpg")
+img5 = Image.open("tkinterpractice\imageviewerapp\img5.jpg")
 img5 = img5.resize((width, height))
 img5 = ImageTk.PhotoImage(img5)
 
-img6 = Image.open("tkinterpractice\img6.jpg")
+img6 = Image.open("tkinterpractice\imageviewerapp\img6.jpg")
 img6 = img6.resize((width, height))
 img6 = ImageTk.PhotoImage(img6)
 
-img7 = Image.open("tkinterpractice\img7.jpg")
+img7 = Image.open("tkinterpractice\imageviewerapp\img7.jpg")
 img7 = img7.resize((width, height))
 img7 = ImageTk.PhotoImage(img7)
 
-img8 = Image.open("tkinterpractice\img8.jpg")
+img8 = Image.open("tkinterpractice\imageviewerapp\img8.jpg")
 img8 = img8.resize((width, height))
 img8 = ImageTk.PhotoImage(img8)
 
-img9 = Image.open("tkinterpractice\img9.jpg")
+img9 = Image.open("tkinterpractice\imageviewerapp\img9.jpg")
 img9 = img9.resize((width, height))
 img9 = ImageTk.PhotoImage(img9)
 
-img10 = Image.open("tkinterpractice\img10.jpg")
+img10 = Image.open("tkinterpractice\imageviewerapp\img10.jpg")
 img10 = img10.resize((width, height))
 img10 = ImageTk.PhotoImage(img10)
 
-img11 = Image.open("tkinterpractice\img11.jpg")
+img11 = Image.open("tkinterpractice\imageviewerapp\img11.jpg")
 img11 = img11.resize((width, height))
 img11 = ImageTk.PhotoImage(img11)
 
@@ -81,12 +81,16 @@ def next():
     if(index>=len(image_list)):
         global right_button
         right_button.config(state = "disabled")
+        global status_bar
+        status_bar.config(text = "Image "+str(index)+" of "+str(len(image_list)))
     else:
         
         global label
         label.grid_forget()
         label = Label( image = image_list[index])
         label.grid(row = 0, column = 1, columnspan=3)
+        
+        status_bar.config(text = "Image "+str(index+1)+" of "+str(len(image_list)))
 
     
 
@@ -100,27 +104,38 @@ def back():
     print(" back index:",index)
     global label
     if index<0:
+        
         global left_button
         left_button.config(state = "disabled")
+        global status_bar
+        
+        status_bar.config(text = "Image "+str(index+2)+" of "+str(len(image_list)))
+
     else:
         
         label.grid_forget()
         label = Label( image = image_list[index])
         label.grid(row = 0, column = 1, columnspan=3)
+        
+        status_bar.config(text = "Image "+str(index+1)+" of "+str(len(image_list)))
 
-    
+###########  status bar adding ###############################
+status_bar = Label( text = "Image "+str(index+1)+" of "+str(len(image_list)) , bd = 2, relief="sunken", anchor = "e")
 
-left_button = Button(text="Back", font = "calibri", bg = "grey", fg = "white",command =back)
-left_button.grid( row = 2, column =1 )
+
+left_button = Button(text="Back", font = "calibri", bg = "gainsboro", fg = "black",command =back)
+left_button.grid( row = 2, column =1, pady = 10)
 
 
 quit_button = Button(text = "Exit program", fg = "white", bg = "salmon", command = root.quit,  padx=15)
 quit_button.config(font = "calibri")
-quit_button.grid(row = 2, column = 2,sticky = "ew")
+quit_button.grid(row = 2, column = 2,sticky = "ew",pady = 10)
 
 
-right_button = Button(text="Next", font = "calibri", bg = "grey", fg = "white", command = next )
-right_button.grid( row = 2, column =3 )
+right_button = Button(text="Next", font = "calibri", bg = "gainsboro", fg = "black", command = next )
+right_button.grid( row = 2, column =3 ,pady = 10)
+
+status_bar.grid(row =3, column=1,columnspan=3, sticky="ew")
 
 
 
